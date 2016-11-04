39af3ea4db32c51addf95e894add17bcd217b1f8
cgpllx <cgpllx1@qq.com>
2016-11-04 10:50:57 +0800

    add progress

---
+++ EasyRecyclerView/src/main/java/cc/easyandroid/easyrecyclerview/core/progress/EasyProgressFrameLayout.java
@@ -0,0 +1,97 @@
+package cc.easyandroid.easyrecyclerview.core.progress;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import cc.easyandroid.easyrecyclerview.EasyRecyclerView;
+import cc.easyandroid.easyrecyclerview.R;
+import cc.easyandroid.easyrecyclerview.core.ProgressEmptyView;
+import cc.easyandroid.easyrecyclerview.listener.OnEasyProgressClickListener;
+
+public class EasyProgressFrameLayout extends FrameLayout {
+
+    private EasyRecyclerView.ProgressHander mProgressHander;
+
+    public EasyProgressFrameLayout(Context context) {
+        this(context, null);
+    }
+
+    public EasyProgressFrameLayout(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, R.attr.EasyProgressLayoutStyle);
+    }
+
+    public EasyProgressFrameLayout(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        ProgressEmptyView progressEmptyView = new ProgressEmptyView(context, attrs, defStyle);
+        setProgressHander(progressEmptyView);
+    }
+
+    public void showLoadingView() {
+        updata(false);
+        mProgressHander.showLoadingView();
+    }
+
+    public void showEmptyView() {
+        updata(false);
+        mProgressHander.showEmptyView();
+    }
+
+    public void showErrorView() {
+        updata(false);
+        mProgressHander.showErrorView();
+    }
+
+    public void showContentView() {
+        updata(true);
+    }
+
+    public void setOnEasyProgressClickListener(OnEasyProgressClickListener listener) {
+        mProgressHander.setOnEasyProgressClickListener(listener);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        // 要添加在window后才能找到parent
+        ViewGroup parent = (ViewGroup) getParent();
+        if (parent == null) {
+            throw new IllegalStateException(getClass().getSimpleName() + " is not attached to parent view.");
+        }
+        if (emptyView != null) {
+            parent.removeView(emptyView);
+            parent.addView(emptyView);
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+
+    public void setProgressHander(EasyRecyclerView.ProgressHander progressHander) {
+        mProgressHander = progressHander;
+        setEmptyView(mProgressHander.getView());
+    }
+
+    private View emptyView;
+
+    void updata(boolean hasData) {
+        if (hasData && emptyView != null) {
+            emptyView.setVisibility(View.GONE);
+            EasyProgressFrameLayout.this.setVisibility(View.VISIBLE);
+        } else {
+            if (emptyView != null) {
+                emptyView.setVisibility(View.VISIBLE);
+                EasyProgressFrameLayout.this.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+    }
+}
